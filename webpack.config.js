0097fea092057095a9ec0317e9623580d52c6b9b
Pedestal19 <gabeoji01@gmail.com>
2020-05-18 02:32:21 +0100

    updated webpack config, service workers working

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,81 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// import WorkboxPlugin from 'workbox-webpack-plugin';
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devHost = "localhost";
+const devPort = "5000";
 
-module.exports = {
-  entry: './src/client/index.js',
+const runningEnvironment = {
+  Development: "development",
+  Production: "production"
+};
+
+const plugins = {
+  [runningEnvironment.Development]: [],
+  [runningEnvironment.Production]: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new WorkboxPlugin.GenerateSW()
+  ]
+};
+
+const stylesUse = {
+  [runningEnvironment.Development]: ["style-loader", "css-loader", "sass-loader"],
+  [runningEnvironment.Production]: [
+    "style-loader",
+    MiniCssExtractPlugin.loader,
+    "css-loader",
+    "sass-loader"
+  ]
+};
+
+module.exports = (env = runningEnvironment.Development) => ({
+  devServer: {
+    host: devHost,
+    port: devPort,
+    proxy: [
+      {
+        context: ["/analyseText", "/api"],
+        target: "http://localhost:5000"
+      }
+    ],
+    hot: true,
+    overlay: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    historyApiFallback: true
+  },
+  mode: env,
+  entry: "./src/client/index.js",
   output: {
-    libraryTarget: 'var',
-    library: 'Client',
+    path: path.join(__dirname, "dist"),
+    filename: "bundle-[hash].min.js",
+    libraryTarget: "var",
+    library: "Client"
   },
   module: {
     rules: [
       {
-        test: '/.js$/',
+        test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
+        test: /\.(sa|sc|c)ss$/,
+        use: stylesUse[env]
+      }
+    ]
   },
   plugins: [
+    new CleanWebpackPlugin({ verbose: true }),
     new HtmlWebPackPlugin({
-      template: './src/client/views/index.html',
-      filename: './index.html',
-    }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
-    
-  ],
-};
+      template: "./src/client/views/index.html",
+      hash: true,
+      xhtml: true
+    })
+  ].concat(plugins[env])
+});
